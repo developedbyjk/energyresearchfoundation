40b2c6dd8f662fdbadd1dec0d1fb1320d2e6498d
developedbyjk <developedbyjk@gmial.com>
2022-06-14 15:52:13 +0530

    just thought to add just one slide type not more

--- script.js
+++ script.js
@@ -1,19 +1,2 @@
 
-var slideIndex = 1;
-show_slide(slideIndex);
 
-function plusSlides(n){
-    show_slide(slideIndex+=1)
-}
-
-function show_slide(n){
-    var slides = document.getElementsByClassName("box");
-    if(n>slides.length){slideIndex = 1}
-    if(n<1){slideIndex=slides.length}
-
-    for(i=0;i<slides.length;i++){
-        slides[i].style.display = "none";
-    }
-
-    slides.[slideIndex-1].style.display = "block";
-}
