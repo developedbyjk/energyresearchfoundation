cbcd5f3b140b11bb3666e83d59b40900a9d3c80c
developedbyjk <developedbyjk@gmial.com>
2022-06-17 12:11:12 +0530

    added faq panel

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 
+// HERS LITTLE JS FOR THE WORKING OF ACCORDIAN
+var button = document.getElementsByClassName("accordion");
+var i;
 
+for(i=0;i<button.length;i++){
+    button[i].addEventListener('click',function(){
+        this.classList.toggle("newclass");
+        var panel = this.nextElementSibling;
+        if(panel.style.maxHeight){
+            panel.style.maxHeight=null;
+        }else{
+             panel.style.maxHeight= panel.scrollHeight + "px";
+        }
+    })
+}
+// END OF SCRIPT OF ACCORDIAN
